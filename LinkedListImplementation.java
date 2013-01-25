cc0b75836bcefe73db85b10e90bde669a5228fdc
vlad101 <vladimir.efros@gmail.com>
2013-01-25 15:19:47 -0500

    An update for LinkedListImplementation.java: all the classes and the interfaces were split into separate java files. The remove method of the LinkedList Class has been improved. The getIterator method has been added to the LinkedList Class. The Iterator Class was added with its interface. The methods (next(), hasNext(), remove()) were added to the Iterator Class.

--- LinkedListImplementation.java
+++ LinkedListImplementation.java
@@ -171,15 +171,15 @@ class LinkedList implements LinkedListType
      */
     public boolean remove(String str)
     {
-        boolean flag = false;
         if(str == null || str.length() < 1)
-            flag = false;
+            return false;
+
         if(first.value.equals(str))
         {
             first = first.next;
             if(first == null)
                 last = null;
-            flag = true;
+            return true;
         }
         else
         {
@@ -187,16 +187,34 @@ class LinkedList implements LinkedListType
             for(pred = first; pred.next != null && !(pred.next.value.equals(str)); pred = pred.next)
                 pred = pred.next;
             if(pred.next == null)
-                flag = false;
+                return false;
             else
             {
                 pred.next = pred.next.next;
                 if(pred.next == null)
                     last = pred;
-                flag = true;
+                return true;
             }
         }
-        return flag;
+    }
+
+    /**
+     * The get method gets the String at the specified location.
+     * @param index THe location of the String that is to be returned.
+     * @return The String at the specified location.
+     */
+    public String get(int index)
+    {
+	if(index < 0 || index > size())
+	    throw new IndexOutOfBoundsException();
+	else
+            {
+		Node ref = first;
+		for(int i = 0; i < size(); i++, ref = ref.next)
+	    	    if(i == index)
+			return ref.value;
+	    }
+	return null;
     }
     
     /**
@@ -217,6 +235,14 @@ class LinkedList implements LinkedListType
             count++;
         return count;
     }
+
+    /*
+     * The getIterator method returns a new Iterator.
+     */
+    public Iterator getIterator()
+    {
+    	return new Iterator(this);
+    }
     
     /**
      * @return The elements of the LinkedList and convert is to string.
@@ -229,6 +255,66 @@ class LinkedList implements LinkedListType
             builder.append(ref.value + " ");
         return builder.toString();
     }
+}
+
+/**
+ * The LinkedList Iterator implementation.
+ */
+class Iterator
+{
+    private LinkedList theList;
+    private boolean canRemove;
+    private int prev;
+	
+    /**
+     * The constructor method initializes the LinkedList Iterator.
+     * @param list Initializes the LinkedList Object that will be iterated.
+     */
+    public Iterator(LinkedList list)
+    {
+	theList = list;
+	canRemove = false;
+	prev = -1;
+    }
+    
+    /**
+     * The hasNext method returns true if the LinkedList element 
+     * is in the boundaries, otherwise returns false.
+     */
+    public boolean hasNext()
+    {
+    	return (prev + 1 < theList.size());
+    }
+    
+    /**
+     * The next method uses the get method of the LinkedList class 
+     * to obtain the value of the String of the LinkedList at the specified location.
+     * @return the String at the specified location.
+     */
+    public String next()
+    {
+    	if(hasNext())
+    	{
+    		canRemove = true;
+    		prev++;
+    		return theList.get(prev);
+    	}
+    	
+    	else
+    		return null;
+    }
+    
+    public void remove()
+    {
+    	if(canRemove)
+    	{
+    		theList.remove(prev);
+    		canRemove = false;
+    		prev--;
+    	}
+    	else
+    		return;
+    }    
 }
 
 /**
@@ -245,15 +331,21 @@ public class LinkedListImplementation
         for(String s : nums)
             myList.add(s);
 
-	// Display the result
+		// Display the result
         System.out.println(myList);
+	
+		// Display the size of the array
+		System.out.println(myList.size());
+	
+		for(int i = 0; i < myList.size(); i++)
+            System.out.println("The String \"" + myList.get(i) + "\" at the position " + i);
         
         // Remove three Strings from the String LinkedList
         myList.remove("One");
         myList.remove("Two");
         myList.remove("Three");
 
-	// Display the result
+		// Display the result
         System.out.println(myList);
         
         // Add three Strings to the String LinkedList at the specified location
@@ -261,20 +353,57 @@ public class LinkedListImplementation
         myList.add(1, "newOne"); 
         myList.add(2, "NewTwo"); 
 
-	// Display the result
+		// Display the result
         System.out.println(myList);
         
-	// Remove two Strings from the String LinkedList at the specified location
+		// Remove two Strings from the String LinkedList at the specified location
         myList.remove(0);
         myList.remove(1);
 
-	// Display the result
+		// Display the result
         System.out.println(myList);
         
-	// Delete all the Strings from the String LinkedList
+		// Delete all the Strings from the String LinkedList
         myList.clear();
 
-	// Display the result
+		// Display the result
         System.out.println(myList);
+        
+    	// ~ An Iterator Test Drive~ 
+    
+		// Add three Strings to the String LinkedList
+		String[] nums1 = {"One", "Two", "Three"};
+		for(String s : nums)
+		    myList.add(s);
+    	
+		// Display the result
+        System.out.println(myList);
+        
+        // Create three new Object Iterator
+        Iterator iterator1 = myList.getIterator();
+        Iterator iterator2 = myList.getIterator();
+        Iterator iterator3 = myList.getIterator();
+        
+        System.out.print ("Printing using the Iterator: ");
+        // Iterate through the LinkedList and get the elemenents of the LinkedList
+        while(iterator1.hasNext())
+        	System.out.print(iterator1.next() + " ");
+        System.out.println();
+        
+        // Iterate through the LinkedList and remove all the elemenents of the LinkedList
+        while(iterator2.hasNext())
+        {
+        	iterator2.next();
+        	iterator2.remove();
+        }
+        
+        // Check the result with the toString method
+        System.out.println(myList);
+                        
+        System.out.print ("Printing using the Iterator: ");
+        // Iterate through the LinkedList and get the elemenents of the LinkedList
+        while(iterator3.hasNext())
+        	System.out.print(iterator3.next() + " ");
+        System.out.println();
     }
 }
